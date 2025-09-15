d98008e6f5b95bc012d88ac22c476b24ee92e289
EYANG6701 <eyang3305@gmail.com>
2025-09-15 14:22:51 -0400

    fulladder

--- GPTChallenges/fulladder.v
+++ GPTChallenges/fulladder.v
@@ -7,7 +7,13 @@ module fulladder (sum, c_out, a, b, c_in);
     input a, b, c_in
 
     //wires
-    wire w1, w2;
+    wire w1, w2, w3;
 
     xor (w1, a, b);
-    
+    and (w2, a, b);
+    xor (sum, c_in, w1);
+    and (w3, w1, c_in);
+    or (c_out, w2, w3);
+endmodule
+
+
