27a17dcf24e014943f56eca9ca016e5b5cb1499c
EYANG6701 <eyang3305@gmail.com>
2025-09-19 00:03:23 -0400

    fixed decoder code

--- Projects/decoder_project/decoder.v
+++ Projects/decoder_project/decoder.v
@@ -1,11 +1,11 @@
 module decoder (
-    input [1:0] in
+    input  [1:0] in,
     output [3:0] out
 );
 
-    assign out[3] = ~(in[1] & in[0]);
-    assign out[2] = in[0];
-    assign out[1] = in[1];
-    assign out[0] = in[1] & in[0];
-    
+    assign out[3] = (in == 2'b00);
+    assign out[2] = (in == 2'b01);
+    assign out[1] = (in == 2'b10);
+    assign out[0] = (in == 2'b11);
+
 endmodule
