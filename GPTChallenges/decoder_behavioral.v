f1ba8e2abf9b7676556505b8e624242184aced5a
EYANG6701 <eyang3305@gmail.com>
2025-09-18 21:10:46 -0400

    fixed bug

--- GPTChallenges/decoder_behavioral.v
+++ GPTChallenges/decoder_behavioral.v
@@ -7,7 +7,7 @@ module decoder_behavioral (
         case (in)
             2'b00: out = 4'b1000;
             2'b01: out = 4'b0100;
-            2'b10: out = 4'0010;
+            2'b10: out = 4'b0010;
             2'b11: out = 4'b0001;
         endcase
     end
