2f8388f01f545bd57df55d0f035d78be1a56959c
EYANG6701 <eyang3305@gmail.com>
2025-09-24 10:37:48 -0400

    d-flip flop

---
+++ Projects/D_Flip_Flop_Project/dflip.v
@@ -0,0 +1,18 @@
+module dflip (q, q_bar, d, clk, reset);
+
+    //Ouput ports
+    output reg q, q_bar;
+
+    //Input ports
+    input d, clk, reset;
+
+    //Behavioral
+    always @ (posedge clk) begin
+        if (reset) begin
+            q <= 1'b0;
+            q_bar <= 1'b1;
+        end else begin
+        q <= d;
+        q_bar <= ~d;
+    end
+endmodule
