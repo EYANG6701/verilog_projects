040f97d3d3bedcdd65d86b0604ee44c9ee491170
EYANG6701 <eyang3305@gmail.com>
2025-10-14 14:53:25 -0400

    extra fixes

--- Misc/ECSE318/Lab2Problem1Correction/set_condition_test.v
+++ Misc/ECSE318/Lab2Problem1Correction/set_condition_test.v
@@ -8,9 +8,9 @@ module set_condition (
         C = 16'b0;
         case (code)
             3'b000: if (A <= B) C = 16'b1;
-            3'b001: if (A <  B) C = 16'b1;
+            3'b001: if (A < B) C = 16'b1;
             3'b010: if (A >= B) C = 16'b1;
-            3'b011: if (A >  B) C = 16'b1;
+            3'b011: if (A > B) C = 16'b1;
             3'b100: if (A == B) C = 16'b1;
             3'b101: if (A != B) C = 16'b1;
             default: C = 16'b0;
